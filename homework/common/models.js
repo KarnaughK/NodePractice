61ba6d4efbaa0d091ee71db1eea4be22462e5032
unknown <>
2016-06-10 21:32:07 +0800

    更改的代码

--- homework/common/models.js
+++ homework/common/models.js
@@ -1,40 +1,43 @@
-
 module.exports = {
     user: {
-        name: { type: String, required: true },
-        password: { type: String, required: true }
+        name: {type: String, required: true},
+        password: {type: String, required: true},
+        level: {type: int, required: true, default: 0}
     },
     commodity: {
         name: String,
         price: Number,
         imgSrc: String
     },
-    cart:{
-        uId: { type: String },
-        cId: { type: String },
-        cName: { type: String },
-        cPrice: { type: String },
-        cImgSrc: { type:String } ,
-        cQuantity: { type: Number },
-        cStatus : { type: Boolean, default: false  }
+    cart: {//记录（购物车&购买记录）
+        uId: {type: String},
+        cId: {type: String},
+        cName: {type: String},
+        cPrice: {type: String},
+        cImgSrc: {type: String},
+        cQuantity: {type: Number},
+        cStatus: {type: Boolean, default: false}//是否购买了
     },
     textbook: {//教材
-        Id:{type:Number,required: true},//教材编号
+        bookId: {type: Number, required: true},//教材编号
         name: String,//教材名
         price: Number,//价格
-        press:String,//出版社
-        supplier:String//供应商
+        press: String,//出版社
+        supplier: String//供应商
     },
     speciality: {//专业
-        Id:{type:Number,required: true},//专业号
         name: String,//专业名
         Grade: Number,//年级
         department: String//院系
     },
-    warehouse:{//仓库
-        wId: { type: String },//仓库号
-        sId: { type: String },//教材编号
-        intime: { type: String },//入库时间
-        outoftime: { type: String }//出库时间
+    warehouse: {//仓库
+        wId: {type: String},//仓库号
+        sId: {type: String},//教材编号
+        intime: {type: String},//入库时间
+        outoftime: {type: String}//出库时间
     }
+    //发放记录
+    //班级 （年级id，院系id）
+    //年级
+    //院系
 };
