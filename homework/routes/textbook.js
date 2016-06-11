c3ecb08cae28339cdd8b55d5ada93e64611925eb
unknown <>
2016-06-11 23:54:52 +0800

    添加删除更改。。。

--- homework/routes/textbook.js
+++ homework/routes/textbook.js
@@ -15,17 +15,42 @@ module.exports = function (app) {
         }
     });
 
+    //新增教材信息
     app.post('/addtextbook', function (req, res) {
-        var User = global.dbHelper.getModel('user');
-        if(!req.body.bookid){
+        var Book = global.dbHelper.getModel('textbook');
+        if(!req.body.bookId){
             res.send({status: false, msg: "请填写教材编号"});
             return;
         }
-        User.create(req.body, function (error, doc) {
+        Book.create(req.body, function (error, doc) {
             if (error) {
                 res.send({status: false, msg: "出错"});
             } else {
                 res.send({status: true, msg: "添加成功!"});
+            }
+        });
+    });
+
+    //删除教材信息
+    app.get("/deletetextbook/:id", function(req, res) {
+        //req.params.id 获取商品ID号
+        var Cart = global.dbHelper.getModel('textbook');
+        Cart.remove({"_id":req.params.id},function(error,doc){
+            //成功返回1  失败返回0
+            if(doc > 0){
+                res.redirect('/textbook');
+            }
+        });
+    });
+
+    //修改教材信息
+    app.post("/updatetextbook", function(req, res) {
+        //req.params.id 获取商品ID号
+        var Cart = global.dbHelper.getModel('textbook');
+        Cart.remove({"_id":req.params.id},function(error,doc){
+            //成功返回1  失败返回0
+            if(doc > 0){
+                res.redirect('/textbook');
             }
         });
     });
