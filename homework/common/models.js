e7d5812733fd31ae2aef634eb55104dcdb190961
karnaugh <292214146@qq.com>
2016-06-11 22:04:00 +0800

    修改数据库

--- homework/common/models.js
+++ homework/common/models.js
@@ -25,19 +25,40 @@ module.exports = {
         press: String,//出版社
         supplier: String//供应商
     },
-    speciality: {//专业
-        name: String,//专业名
-        grade: Number,//年级
-        department: String//院系
+    department: {//院系
+        name: String//院系名
     },
-    warehouse: {//仓库
-        wId: {type: String},//仓库号
-        sId: {type: String},//教材编号
-        intime: {type: String},//入库时间
-        outoftime: {type: String}//出库时间
+    profession: {//专业
+        name: String,//专业名
+        departmentId: String,//院系id
+        departmentName: String//院系名称
+    },
+    grade: {//年级
+        name: String,//年级名
+        grade: Number,//年级
+        professionId: String,//专业id
+        professionName: String,//专业名称
+        departmentId: String,//院系id
+        departmentName: String//院系名称
+    },
+    class: {//班级
+        name: String,//班级名
+        gradeId: String,//年级id
+        gradeName: String,//年级名称
+        professionId: String,//专业id
+        professionName: String,//专业名称
+        departmentId: String,//院系id
+        departmentName: String//院系名称
+    },
+    stock: {//库存表
+        bookId: {type: String},//教材编号
+        time: {type: String},//时间
+        number: {type: Number},//数量
+        operation: {type: Number}//操作
+    },
+    record: {//发放记录
+
+
     }
     //发放记录
-    //班级 （年级id，院系id）
-    //年级
-    //院系
 };
