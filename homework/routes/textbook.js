ab56f11b6b531c54f33e1ae01b87c7c5ef7cd7bf
karnaugh <292214146@qq.com>
2016-06-12 02:08:05 +0800

    add

--- homework/routes/textbook.js
+++ homework/routes/textbook.js
@@ -16,9 +16,9 @@ module.exports = function (app) {
     });
 
     //新增教材信息
-    app.post('/addtextbook', function (req, res) {
+    app.post('/textbook/add', function (req, res) {
         var Book = global.dbHelper.getModel('textbook');
-        if(!req.body.bookId){
+        if (!req.body.bookId) {
             res.send({status: false, msg: "请填写教材编号"});
             return;
         }
@@ -32,25 +32,58 @@ module.exports = function (app) {
     });
 
     //删除教材信息
-    app.get("/deletetextbook/:id", function(req, res) {
+    app.get("/textbook/delete/:id", function (req, res) {
         //req.params.id 获取商品ID号
         var Cart = global.dbHelper.getModel('textbook');
-        Cart.remove({"_id":req.params.id},function(error,doc){
+        Cart.remove({"_id": req.params.id}, function (error, doc) {
             //成功返回1  失败返回0
-            if(doc > 0){
+            if (doc > 0) {
                 res.redirect('/textbook');
+            }
+        });
+    });
+    //查找教材信息
+    app.post("/textbook/select", function (req, res) {
+        //req.params.id 获取商品ID号
+        var Cart = global.dbHelper.getModel('textbook');
+        Cart.findOne({"_id": req.body._id}, function (error, doc) {
+            if (error) {
+                //res.send是返回数据
+                //约定返回值格式  status：返回是否成功 msg:消息 data:返回数据
+                res.send({status: false, msg: error});
+            } else if (doc) {
+                res.send({status: true, msg: "查询成功", data: doc});
+            } else {
+                res.send({status: false, msg: "没有此书籍"});
             }
         });
     });
 
     //修改教材信息
-    app.post("/updatetextbook", function(req, res) {
+    app.post("/textbook/update", function (req, res) {
         //req.params.id 获取商品ID号
         var Cart = global.dbHelper.getModel('textbook');
-        Cart.remove({"_id":req.params.id},function(error,doc){
+        Cart.remove({"_id": req.params.id}, function (error, doc) {
             //成功返回1  失败返回0
-            if(doc > 0){
+            if (doc > 0) {
                 res.redirect('/textbook');
+            }
+        });
+        Cart.findOne({"_id": req.body._id}, function (error, doc) {
+            if (error) {
+                //res.send是返回数据
+                //约定返回值格式  status：返回是否成功 msg:消息 data:返回数据
+                res.send({status: false, msg: error});
+            } else if (doc) {
+                doc.bookId = req.body.bookId;
+                doc.name = req.body.name;
+                doc.press = req.body.press;
+                doc.price = req.body.price;
+                doc.supplier = req.body.supplier;
+                doc.save();
+                res.send({status: true, msg: "修改成功"});
+            } else {
+                res.send({status: false, msg: "没有此书籍"});
             }
         });
     });
